41b799761cdca0437f17f2401de096e807acdc79
beartest <3131613283@qq.com>
2016-04-28 12:50:39 +0800

    Added files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO;
+using ESRI.ArcGIS.Client;
+using ESRI.ArcGIS.Client.Tasks;
+using ESRI.ArcGIS.Client.Geometry;
+using ESRI.ArcGIS.Client.Symbols;
+using System.Json;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using geometry_json_translate.MyServiceReference1;
+
+namespace geometry_json_translate
+{
+    public partial class MainPage : UserControl
+    {
+        ShapeFile shapeFileReader = new ShapeFile();
+        string names = null;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        #region      按钮变色
+        private void textBlock1_MouseEnter(object sender, MouseEventArgs e)
+        {
+            rectangle4.Fill = new SolidColorBrush(Color.FromArgb(119, 185, 185, 185));
+            textBlock1.Foreground = new SolidColorBrush(Colors.White);
+        }
+
+        private void rectangle4_MouseEnter(object sender, MouseEventArgs e)
+        {
+            rectangle4.Fill = new SolidColorBrush(Color.FromArgb(119, 185, 185, 185));
+            textBlock1.Foreground = new SolidColorBrush(Colors.White);
+        }
+
+        private void rectangle4_MouseLeave(object sender, MouseEventArgs e)
+        {
+            rectangle4.Fill = new SolidColorBrush(Color.FromArgb(119, 145, 145, 145));
+            textBlock1.Foreground = new SolidColorBrush(Color.FromArgb(225, 220, 220, 220));
+        }
+        #endregion
+
+        private void MyClient1_DynamicAccessCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("成功生成表");
+        }
+
+        private void MyClient2_MyInsertCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+        }
+
+        //打开
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox1.Text == null)
+            {
+                MessageBox.Show("数据库名不能为空");
+                return;
+            }
+            if (textBox2.Text == null)
+            {
+                MessageBox.Show("表名不能为空");
+                return;
+            }
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;
+            dialog.InitialDirectory = "E:\\0cuijing\\geometry-json-translate-属性保存\\shapefile";
+            dialog.Filter = "Shape Files (*.shp;*.dbf)|*.shp;*.dbf";
+            dialog.ShowDialog();
+            if (dialog.Files == null)
+                return;
+            //判断拖放的文件是否为.shp和.dbf
+            FileInfo shapefile = null;
+            FileInfo dbffile = null;
+            foreach (FileInfo fi in dialog.Files)
+            {
+                if (fi.Extension.ToLower() == ".shp")
+                    shapefile = fi;
+                if (fi.Extension.ToLower() == ".dbf")
+                    dbffile = fi;
+            }
+            // 读取Shapefile数据  
+            if (shapefile != null && dbffile != null)
+            {
+                shapeFileReader.Read(shapefile, dbffile);
+            }
+            else
+            {
+                MessageBox.Show("请同时选择.dbf和.shp文件");
+                return;
+            }
+        }
+        //建立表
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            ShapeFileRecord record1 = shapeFileReader.Records[0];
+            Graphic g1 = record1.ToGraphic();
+            if (g1 != null)
+            {
+                FeatureSet fs1 = new FeatureSet();
+                fs1.Features.Add(g1);
+                string json1 = fs1.ToJson();
+                JsonObject jsonObj1 = JsonObject.Parse(json1) as JsonObject;
+                if (jsonObj1.ContainsKey("features"))
+                {
+                    JsonValue jsonFea1 = jsonObj1["features"];
+                    if (jsonFea1 is JsonArray)
+                    {
+                        if (jsonFea1[0].ContainsKey("attributes"))
+                        {
+                            JsonValue jsonAtt1 = jsonFea1[0]["attributes"];
+                            JsonObject jsonAO1 = jsonAtt1 as JsonObject;
+                            ArrayOfString Names = new ArrayOfString();
+                            ArrayOfString Types = new ArrayOfString();
+                            //获取名
+                            foreach (string name in jsonAO1.Keys)
+                            {
+                                Names.Add(name);
+                                names += "," + name;
+                            }
+                            Names.Add("Json");
+                            names += ",Json";
+                            //获取属性类型
+                            foreach (JsonValue item in jsonAO1.Values)
+                            {
+                                switch (item.JsonType)
+                                {
+                                    case JsonType.String:
+                                        Types.Add((typeof(String)).ToString());
+                                        break;
+                                    case JsonType.Number:
+                                        Types.Add((typeof(Double)).ToString());
+                                        break;
+                                    case JsonType.Boolean:
+                                        Types.Add((typeof(Boolean)).ToString());
+                                        break;
+                                    case JsonType.Array:
+                                        Types.Add((typeof(Array)).ToString());
+                                        break;
+                                    case JsonType.Object:
+                                        Types.Add((typeof(Object)).ToString());
+                                        break;
+                                    default: MessageBox.Show("未识别类型");
+                                        break;
+                                }
+                            }
+                            Types.Add("Json");
+                            MyWebService1SoapClient MyClient1 = new MyWebService1SoapClient();
+                            MyClient1.DynamicAccessCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(MyClient1_DynamicAccessCompleted);
+                            MyClient1.DynamicAccessAsync(textBox1.Text + ".mdb", textBox2.Text, Names, Types);
+                        }
+                    }
+                }
+            }
+        }
+        //保存属性
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            Graphic g = new Graphic();
+            foreach (ShapeFileRecord record in shapeFileReader.Records)
+            {
+                g = record.ToGraphic();
+                if (g != null)
+                {
+                    FeatureSet fs = new FeatureSet();
+                    fs.Features.Add(g);
+                    string json = fs.ToJson();
+                    JsonObject jsonObj = JsonObject.Parse(json) as JsonObject;
+                    if (jsonObj.ContainsKey("features"))
+                    {
+                        JsonValue jsonFea = jsonObj["features"];
+                        if (jsonFea is JsonArray)
+                        {
+                            for (int i = 0; i < jsonFea.Count; i++)
+                            {
+                                if (jsonFea[i].ContainsKey("attributes"))
+                                {
+                                    JsonValue jsonAtt = jsonFea[i]["attributes"];
+                                    JsonObject jsonAO = jsonAtt as JsonObject;
+                                    string values = "";
+                                    foreach (JsonValue item in jsonAO.Values)
+                                    {
+                                        string s = item.ToString();
+                                        s = s.Substring(0, s.Length - 1);
+                                        s = s.Substring(1);
+                                        switch (item.JsonType)
+                                        {
+                                            case JsonType.String:
+                                                values += ",'" + s.Trim() + "'";
+                                                break;
+                                            case JsonType.Number:
+                                            case JsonType.Boolean:
+                                            case JsonType.Array:
+                                            case JsonType.Object:
+                                                values += "," + s.Trim();
+                                                break;
+                                        }
+                                    }
+                                    values += ",'" + json + "'";
+                                    MyWebService1SoapClient MyClient2 = new MyWebService1SoapClient();
+                                    MyClient2.MyInsertCompleted += new EventHandler<System.ComponentModel.AsyncCompletedEventArgs>(MyClient2_MyInsertCompleted);
+                                    MyClient2.MyInsertAsync(names, values, textBox1.Text + ".mdb", textBox2.Text);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ MyDataGrid.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Threading;
+
+namespace geometry_json_translate
+{
+    public class DataColumn    
+    {
+        #region "Properties"     
+        /// <summary>        
+        /// 列名        
+        /// </summary>        
+        public string ColumnName { get; set; }     
+        /// <summary>      
+        /// 类型        
+        /// </summary>       
+        public Type DataType { get; set; }        
+        /// <summary>       
+        /// 列标题       
+        /// </summary>        
+        public string Caption { get; set; }      
+        /// <summary>        
+        /// 是否允许用户改变列的大小       
+        /// </summary>      
+        public bool AllowResize { get; set; }        
+        /// <summary>        
+        /// 是否允许用户进行排序       
+        /// </summary>       
+        public bool AllowSort { get; set; }       
+        /// <summary>        
+        /// 是否允许用户进行重新排序       
+        /// </summary>        
+        public bool AllowReorder { get; set; }      
+        /// <summary>      
+        /// 是否只读       
+        /// </summary>      
+        public bool ReadOnly { get; set; }        
+        #endregion     
+  
+        /// <summary>       
+        /// 构造并且赋初始值      
+        /// </summary>       
+        /// <param name="columnName">列名</param>     
+         public DataColumn(string columnName)      
+         {          
+             this.ColumnName = columnName;      
+             this.Caption = columnName;          
+             this.AllowResize = true;        
+             this.AllowSort = true;          
+             this.AllowReorder = true;       
+             this.ReadOnly = false;      
+         }      
+        ///<summary>        
+        ///重载构造     
+        ///</summary>       
+        ///<param name="columnName">列名</param>       
+        ///<param name="caption">列标题</param>       
+        ///<param name="allowResize">是否允许改变列大小</param>    
+        ///<param name="allowSort">是否允许排序</param>      
+        ///<param name="allowReorder">是否允许重新排序</param>       
+        ///<param name="readOnly">列只读</param>        
+        public DataColumn(string columnName, string caption, bool allowResize, bool allowSort, bool allowReorder, bool readOnly)      
+        {            
+            this.ColumnName = columnName;        
+            this.Caption = caption;           
+            this.AllowResize = allowResize;     
+            this.AllowSort = allowSort;        
+            this.AllowReorder = allowReorder;   
+            this.ReadOnly = readOnly;       
+        }   
+    }
+
+    /// <summary>  
+    /// DataColumn集合，继承与list  
+    /// </summary>  
+    public class DataColumnCollection : List<DataColumn>   
+    {       
+        /// <summary>      
+        /// 隐藏List类中add方法，重新定义Add方法,判断有重复列的时候报出异常       
+        /// </summary>       
+        /// <param name="dc"></param>        
+        public new void Add(DataColumn dc)   
+        {          
+            foreach (DataColumn curColumn in this)      
+            {            
+                if (dc.ColumnName == curColumn.ColumnName)     
+                {                  
+                    throw new Exception(String.Format("该列已经存在", dc.ColumnName));               
+                }          
+            }          
+            base.Add(dc);        
+        }    
+    }
+
+    public class DataRow  
+    {      
+        public Dictionary<string, object> items { set; get; }      
+        public DataRow()       
+        {         
+            this.items = new Dictionary<string, object>();     
+        }      
+        /// <summary>       
+        /// DataRow类索引器 （DataRow[.....]）     
+        /// </summary>      
+        /// <param name="key"></param>      
+        /// <returns></returns>       
+        public object this[string key]   
+        {          
+            set { items[key] = value; }    
+            get { return items[key]; }     
+        }        
+        /// <summary>      
+        /// 通过emit反射在内存中创建出一个包含属性的类      
+        /// </summary>       
+        /// <returns></returns>    
+        public Assembly EmitAssembly()       
+        {           
+            AssemblyName assemblyName = new AssemblyName("DataRowAssembly");   
+            AssemblyBuilder assemblyBuilder=Thread.GetDomain().DefineDynamicAssembly(assemblyName,AssemblyBuilderAccess.Run);            
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("DataRowModel",true);            TypeBuilder typeBuilder = moduleBuilder.DefineType("DataRowObject",TypeAttributes.Public|TypeAttributes.Class);        
+            foreach(KeyValuePair<string, object>  pair in items)             
+            {           
+                BuilderFieldsAndProperty(typeBuilder, pair.Key,pair.Value.GetType());       
+            }            
+            typeBuilder.CreateType();     
+            return assemblyBuilder;       
+        }        
+        /// <summary>        
+        /// 通过emit反射创建字段和属性      
+        /// </summary>       
+        /// <param name="myTypeBuilder">TypeBuilder</param>       
+        /// <param name="name">需要创建的属性名</param>      
+        /// <param name="type">包含该属性的类的类型</param>      
+        public void BuilderFieldsAndProperty(TypeBuilder myTypeBuilder, string name, Type type)         
+        {         
+            FieldBuilder myFieldBuilder = myTypeBuilder.DefineField(name, type, FieldAttributes.Private); 
+            PropertyBuilder myPropertyBuilder = myTypeBuilder.DefineProperty(name.ToUpper(), PropertyAttributes.HasDefault, type, null);           
+            MethodAttributes getSetAttr = MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig;          
+            MethodBuilder getMethodBuilder = myTypeBuilder.DefineMethod("get_" + name, getSetAttr, type, Type.EmptyTypes);         
+            ILGenerator custNameGetIL = getMethodBuilder.GetILGenerator();       
+            custNameGetIL.Emit(OpCodes.Ldarg_0);        
+            custNameGetIL.Emit(OpCodes.Ldfld, myFieldBuilder);          
+            custNameGetIL.Emit(OpCodes.Ret);        
+            MethodBuilder setMethodBuilder = myTypeBuilder.DefineMethod("set_" + name, getSetAttr, null, new Type[] { type });          
+            ILGenerator custNameSetIL = setMethodBuilder.GetILGenerator();       
+            custNameSetIL.Emit(OpCodes.Ldarg_0);          
+            custNameSetIL.Emit(OpCodes.Ldarg_1);        
+            custNameSetIL.Emit(OpCodes.Stfld, myFieldBuilder);       
+            custNameSetIL.Emit(OpCodes.Ret);         
+            myPropertyBuilder.SetGetMethod(getMethodBuilder);        
+            myPropertyBuilder.SetSetMethod(setMethodBuilder);      
+        }   
+    }
+
+    public class DataRowCollection : List<DataRow> { }
+
+    public class DataTable    
+    {      
+        /// <summary>  
+        /// /// DataTable 的名字       
+        /// /// </summary>      
+        public string Name { get; set; }  
+        /// <summary>       
+        /// /// DataRow的集合   
+        /// /// </summary>     
+        public DataRowCollection Rows { get; set; }  
+        /// <summary>    
+        /// DataColumn的集合      
+        /// </summary>    
+        public DataColumnCollection  Columns  { get; set; }   
+        /// <summary>    
+        /// 构造函数并且赋初始值或创建对象        
+        /// </summary>        
+        /// <param name="name"></param>   
+        public DataTable(string name )       
+        {          
+            this.Name = name;     
+            this.Rows = new DataRowCollection();   
+            this.Columns = new DataColumnCollection(); 
+        }  
+    }
+
+    /// <summary>  
+    /// 自定义MyDataGrid 继承自DataGrid    
+    /// </summary>   
+    public class MyDataGrid:DataGrid   
+    {      
+        public MyDataGrid()     
+        {          
+            //重新定义触发AutoGeneratingColumn时的创建列的方法      
+            this.AutoGeneratingColumn += new EventHandler<DataGridAutoGeneratingColumnEventArgs>           
+                (            
+                (o, e) =>                  
+                {                     
+                    //将dataSource赋给自定义的Datatable             
+                    DataTable dt = ((DataTable)this.DataSoruce);             
+                    //通过自定义DataColumn设置对dataGrid的Cloumn进行相应的修改    
+                    foreach (DataColumn dc in dt.Columns)              
+                    {                         
+                        if (dc.ColumnName.ToUpper() == e.Column.Header.ToString())  
+                        {                       
+                            e.Column.Header = dc.Caption;          
+                            e.Column.IsReadOnly = dc.ReadOnly;   
+                            e.Column.CanUserResize = dc.AllowResize;  
+                            e.Column.CanUserSort = dc.AllowSort;  
+                            e.Column.CanUserReorder = dc.AllowReorder;   
+                            break;                    
+                        }            
+                    }                                
+                }          
+                );      
+        }       
+        public object DataSoruce { set; get; }       
+        /// <summary>       
+        /// 将DataTable转换成list<object>    
+        /// </summary>       
+        /// <param name="table">自定义DataTable</param>     
+        /// <returns></returns>      
+        public List<object> GetDataFromDataTable(DataTable  table )       
+        {          
+            List<object> list = new List<object>();   
+            foreach (DataRow row in table.Rows)         
+            {          
+                Assembly rowAssembly = row.EmitAssembly();   
+                object c=rowAssembly.CreateInstance("DataRowObject");      
+                Type type = rowAssembly.GetType("DataRowObject");        
+                foreach (string key in row.items.Keys)           
+                {                 
+                    PropertyInfo properInfo = type.GetProperty(key.ToUpper());  
+                    properInfo.SetValue(c, row.items[key], null);        
+                }               
+                list.Add(c);          
+            }           
+            return list;         
+        }      
+        public void DataBind()   
+        {           
+            this.ItemsSource = this.GetDataFromDataTable((DataTable)DataSoruce); 
+        }   
+    }
+
+}
